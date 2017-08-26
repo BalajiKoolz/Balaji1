dc8f92ce2e6b41a5f545fa3eb603581dfc697c2e
BalajiKoolz <30749368+BalajiKoolz@users.noreply.github.com>
2017-08-26 16:55:55 +0530

    Update bala3.c

--- bala3.c
+++ bala3.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
-int main()
+void main()
 {
-char c;
+char z;
 int x,y;
-printf("Enter the alphabet");
-scanf("%c",&c);
-x=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
-y=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
+printf("Enter the alphabetic letters: ");
+scanf("%c",&z);
+x=(z=='a'||z=='e'||z=='i'||z=='o'||z=='u');
+y=(z=='A'||z=='E'||z=='I'||z=='O'||z=='U');
 if(x||y)
-printf("%c is vowel",c);
+printf("%c is vowel.",z);
 else
-printf("%c is consotant",c);
-return 0;
+printf("%c is consonant.",z);
 }
